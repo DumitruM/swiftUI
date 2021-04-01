a60a93389a19e649c9a1578f3ce0acadf68a37e6
Mihai Dumitru <mihai.dumitru@viacomcontractor.com>
2021-04-01 20:38:16 +0300

    Initial Commit

---
+++ AlimentsApp/AlimentsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlimentsAppApp.swift
+//  AlimentsApp
+//
+//  Created by mihai.dumitru on 01/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct AlimentsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
